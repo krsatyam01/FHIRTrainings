e33096f1b265406b192deb4479ed346d5af84031
krsatyam01 <42486297+krsatyam01@users.noreply.github.com>
2025-07-17 15:25:06 +0530

    Update vite.config.js

--- fhir-app/vite.config.js
+++ fhir-app/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FHIRTrainings/fhir-app/'
+  base: '/FHIRTrainings/fhir-app/',
+  root: 'src'
 })
